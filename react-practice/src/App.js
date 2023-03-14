172deacab45fa2394afbbde8d4034d87d8dbe098
roopahn184 <rooparoopahn184@gmail.com>
2023-03-14 14:35:25 +0530

    ok

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,22 +1,8 @@
-import React from "react";
-import Navbar from "./Navbar/Navbar";
-import Message from "./component/Message";
-import Product from "./component/Product";
-import Footer from "./component/Footer";
-import Hero from "./Hero/Hero";
+import React from 'react'
+
 function App(){
-  return <>
-    <Navbar />
-    <hr />
-    <Hero />
-    <hr/>
-  <h1>HELLOOOO</h1>
-  <Message/>
-  <hr />
-  <Product />
-  <hr />
-  <Footer />
-  <hr />
-  </>
+    return <>
+    <h1>hloooo</h1>
+    </>
 }
 export default App
