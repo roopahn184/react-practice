fd919131a6c18b58221bd99f62a1993382d11236
roopahn184 <rooparoopahn184@gmail.com>
2023-03-25 12:44:26 +0530

    ok

---
+++ react-practice/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+const Page = (
+   <div className="navbar navbar-dark bg-dark navbar-Expand-lg">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207" width="60px" />
+   <ul className="navbar-nav text-white">
+    <li className="nav-list">pricing</li>
+    <li className="nav-list ">About</li>
+    <li className="nav-list">contact</li>
+   </ul>
+   </div>
+)
+
+ReactDOM.render(Page,document.getElementById("root"))
+
+
+
+
+
+
+
+
