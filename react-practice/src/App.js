2c838298f05badd4a573c49a0c17768daa159cdd
roopahn184 <rooparoopahn184@gmail.com>
2023-08-17 10:48:12 +0530

    ok

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,38 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
-function App(){
-    const [user, setusers] = useState([])
-   useEffect(()=>{
-    Axios.get("https://jsonplaceholder.typicode.com/todos")
-    .then((res)=>{
-        setusers(res.data)  
-        console.log(res.data.todos,"res")
-    }) .catch(()=>{})
-   })
-    return <>
-     <table>
-        <thead> 
-            <tr>
-                <th>id</th>
-                <th>title</th>
-                <th>completed</th>
-            </tr>
-        </thead>
-        <tbody>     
-           {
-            user.map((item)=>{
-             return   <tr>
-                    <td>{item.id}</td>
-                    <td>{item.title}</td>
-                    <td>{item.completed ? true:false}</td>
-                </tr>
-            })
-           }
-        </tbody>
-     </table>
-    </>
-}
-    export default App
+import React from 'react';
+// import Api  from './component/Api';
+import Form from './component/Form';
 
-  
-  
+ function App (){
+    return (
+      <div>
+        {/* <Api /> */}
+        <Form/>
+      </div>
+    )
+ }export default App
---
+++ react-practice/src/component/Form.jsx
@@ -0,0 +1,35 @@
+import React,{useState}  from "react";
+import '../component/Style.css';
+  
+function Form (){
+    const[formData, setFormData]=useState({
+        name:'',
+        email:'',
+        message:''
+
+    });
+    const handleInputChange =(event)=>{
+        const {name, value} = event.target;
+        setFormData({
+            ...formData,
+            [name]:value,
+        })
+    }
+    const handleSubmit =(event)=>{
+        event.preventDefault();
+        console.log('form submitted:', formData);
+    }
+
+    return(
+        <form onSubmit={handleSubmit} className="Form-handling">
+            <label>Name:</label>
+            <input type="text" name="name" value={formData.name} onChange={handleInputChange} /><br />
+            <label>Email:</label>
+            <input type="email" name="email" value={formData.email} onChange={handleInputChange} /><br />
+            <label>Message</label>
+            <textarea name="message" value={formData.name} onChange={handleInputChange} /><br />
+            <button type="submit">submit</button>
+           
+        </form>
+    )
+}export default Form
